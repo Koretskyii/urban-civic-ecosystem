6f20ebbbfb5e51d9eac67f79e905664344f5e94c
Koretskyi <yuriykoretskiy999@gmail.com>
2026-05-10 15:53:04 +0300

    refactor: remove redundant City store

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,6 +1,4 @@
 export { useAuthStore } from './useAuthStore';
 export { useAppStore } from './useAppStore';
-export { useCityStore } from './useCityStore';
 
-export type { City } from './useCityStore';
 export type { Notification } from './useAppStore';
