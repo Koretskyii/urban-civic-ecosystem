5d73d0fe36697d463bb8b529826ed8f0ed85b2f2
Koretskyi <yuriykoretskiy999@gmail.com>
2026-05-21 09:17:17 +0300

    chore(client): remove unused translation call from city create page

--- client/src/app/[locale]/city/create/page.tsx
+++ client/src/app/[locale]/city/create/page.tsx
@@ -1,9 +1,6 @@
 import { CityInitForm } from '../components/CityInitForm';
-import { getTranslations } from 'next-intl/server';
 
 export default async function CreateCityPage() {
-  const t = await getTranslations();
-
   return (
     <div className="container mx-auto py-10">
       <CityInitForm />
