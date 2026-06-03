998db380e93c0d84435681e9e201d6ec3c32a753
Koretskyi <yuriykoretskiy999@gmail.com>
2026-06-03 12:43:34 +0300

    Configure Prisma connection pooling

--- server/src/prisma/prisma.service.ts
+++ server/src/prisma/prisma.service.ts
@@ -1,19 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
-  constructor(private configService: ConfigService) {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor(configService: ConfigService) {
+    const connectionString = configService.get<string>('db.url');
+    if (!connectionString) {
+      throw new Error('DATABASE_URL is not configured');
+    }
+
     const adapter = new PrismaPg({
-      connectionString: configService.get<string>('db.url'),
+      connectionString,
+      max: configService.get<number>('db.poolMax') ?? 5,
+      connectionTimeoutMillis:
+        configService.get<number>('db.connectionTimeoutMs') ?? 10_000,
+      idleTimeoutMillis:
+        configService.get<number>('db.idleTimeoutMs') ?? 30_000,
+      query_timeout: configService.get<number>('db.queryTimeoutMs') ?? 15_000,
     });
+
     super({
       adapter,
     });
   }
+
   async onModuleInit() {
     await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
